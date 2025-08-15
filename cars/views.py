987edddb5d876cfbab7c75504bb0d285a9efb67f
rodsantis <desantisrod@gmail.com>
2025-08-15 11:14:31 +0200

    Worked on the end-user page, created a template folder on the Core project to use the layout across the system users and worked on the functionality of the view for the end-user

--- cars/views.py
+++ cars/views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
+from cars.models import Car
 
 def cars_view(request):
+    search = request.GET.get('search')
+
+    if not search:
+        cars = Car.objects.all()
+    else:
+        cars = Car.objects.filter(model__icontains=search)
+
     return render(
         request,
         'cars.html',
-        {'cars': {'model': 'Astra 2.0'}}
+        {'cars': cars}
         )
