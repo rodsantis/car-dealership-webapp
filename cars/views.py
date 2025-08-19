4fd5904b402cf6ed39db60df079085059d711272
rodsantis <desantisrod@gmail.com>
2025-08-19 04:52:30 +0200

    Created a page where the user can register a new car, created the forms.py file with its forms and some validations. Finally updated the href of the navbar on layout.html

--- cars/views.py
+++ cars/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from cars.models import Car
+from cars.forms import CarModelForm
 
 def cars_view(request):
     search = request.GET.get('search')
@@ -14,3 +15,18 @@ def cars_view(request):
         'cars.html',
         {'cars': cars}
         )
+
+def new_car_view(request):
+    if request.method == "POST":
+        new_car_form = CarModelForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('cars_list')
+    else:
+        new_car_form = CarModelForm()
+    
+    return render(
+        request,
+        'new_car.html',
+        { 'new_car_form': new_car_form }
+    )
