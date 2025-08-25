f8cd639e75304c774e69882f2f3648105ab4a7ec
rodsantis <desantisrod@gmail.com>
2025-08-25 12:01:29 +0200

    Created the app to manage account creation and login. Created the registration page and implemented the form.

---
+++ accounts/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+def register_view(request):
+    if request.method == "POST":
+        user_form = UserCreationForm(request.POST)
+        if user_form.is_valid():
+            user_form.save()
+            return redirect('login')
+    else:
+        user_form = UserCreationForm()
+    return render (
+        request,
+        'register.html',
+        {'user_form': user_form}
+    )
