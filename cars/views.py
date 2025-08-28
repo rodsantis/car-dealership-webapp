2b532f7c57a6d38ed2848ab6207e5f45d66fbfba
rodsantis <desantisrod@gmail.com>
2025-08-28 13:21:23 +0200

    Updated the CarsLis and the NewCarView from the basic Views CBC to the ListView and CreateView respectively

--- cars/views.py
+++ cars/views.py
@@ -1,43 +1,24 @@
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.shortcuts import render, redirect
-from django.views import View
+from django.views.generic import ListView, CreateView
 
 
-class CarsView(View):  
+class CarsListView(ListView):
+    model = Car
+    template_name = 'cars.html'
+    context_object_name = 'cars'
 
-    def get(self, request):
-        search = request.GET.get('search')
-
-        if not search:
-            cars = Car.objects.all()
-        else:
-            cars = Car.objects.filter(model__icontains=search)
-
-        return render(
-            request,
-            'cars.html',
-            {'cars': cars}
-            )
+    def get_queryset(self):
+        cars = super().get_queryset().order_by('model')
+        search = self.request.GET.get('search')
+        
+        if search:
+            cars = cars.filter(model__icontains=search)
+        return cars
     
 
-class NewCarView(View):
-
-    def get(self, request):
-        new_car_form = CarModelForm()
-        return render(
-        request,
-        'new_car.html',
-        { 'new_car_form': new_car_form }
-    )
-
-    def post(self, request):
-        new_car_form = CarModelForm(request.POST, request.FILES)
-        if new_car_form.is_valid():
-            new_car_form.save()
-            return redirect('cars_list')
-        return render(
-            request,
-            'new_car.html',
-            { 'new_car_form': new_car_form }
-        )
+class NewCarView(CreateView):
+    model = Car
+    form_class = CarModelForm
+    template_name = 'new_car.html'
+    success_url = '/cars/'
