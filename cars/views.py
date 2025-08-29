9e857fec8e723a49efff765b6e2edbcecca5e5ca
rodsantis <desantisrod@gmail.com>
2025-08-29 15:41:57 +0200

    Created the DeleteView and updated some htmls file to route the user when performing action like deleting, updating and cancelling deletion

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,7 @@
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.urls import reverse_lazy
+from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 
 class CarsListView(ListView):
@@ -33,4 +34,12 @@ class CarUpdateView(UpdateView):
     model = Car
     form_class = CarModelForm
     template_name = 'car_update.html'
+
+    def get_success_url(self):
+        return reverse_lazy('car_detail', kwargs={'pk': self.object.pk})
+
+
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = 'car_delete.html'
     success_url = '/cars/'
--- core/urls.py
+++ core/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import NewCarView, CarsListView, CarDetailView, CarUpdateView
+from cars.views import NewCarView, CarsListView, CarDetailView, CarUpdateView, CarDeleteView
 from accounts.views import register_view, login_view, logout_view
 
 urlpatterns = [
@@ -30,4 +30,5 @@ urlpatterns = [
     path('new_car/', NewCarView.as_view(), name='new_car'),
     path('cars/<int:pk>/', CarDetailView.as_view(), name='car_detail'),
     path('cars/<int:pk>/update', CarUpdateView.as_view(), name='car_update'),
+    path('cars/<int:pk>/delete', CarDeleteView.as_view(), name='car_delete'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
