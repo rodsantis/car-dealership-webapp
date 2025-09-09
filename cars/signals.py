c477fcb810192e5418a245f177c245aeb2534eed
rodsantis <desantisrod@gmail.com>
2025-09-09 10:16:29 +0200

    Created a pre_save function on Signals to add a Description/Bio for a Car that is added without one using the API from OpenAI

--- cars/signals.py
+++ cars/signals.py
@@ -1,7 +1,8 @@
 from cars.models import Car, CarInventory
 from django.db.models import Sum
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import pre_save, post_save, post_delete
 from django.dispatch import receiver
+from openai_api.client import get_car_ai_bio
 
 
 def car_inventory_update():
@@ -13,6 +14,15 @@ def car_inventory_update():
         cars_count=cars_count,
         cars_value=cars_value
     )
+
+
+@receiver(pre_save, sender=Car)
+def car_pre_save(sender, instance, **kwargs):
+    if not instance.bio:
+        ai_bio = get_car_ai_bio(
+            instance.model, instance.brand, instance.model_year
+        )
+        instance.bio = ai_bio
 
 
 @receiver(post_save, sender=Car)
