79780f139a35c2a29f05650132ad9bd52c077b2b
rodsantis <desantisrod@gmail.com>
2025-09-15 06:56:24 +0200

    added dotenv support for easier deployment

--- core/settings.py
+++ core/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 import os
+from dotenv import load_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
